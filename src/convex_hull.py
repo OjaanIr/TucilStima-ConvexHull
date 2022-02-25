f80c0cb2bd293ee0b670ee770836d63fca22aaa6
OjaanIr <farnasrozaan7@gmail.com>
2022-02-25 17:51:36 +0700

    need adjusting, fixing, and testing

--- src/convex_hull.py
+++ src/convex_hull.py
@@ -1,4 +1,25 @@
 import util
 
-# def myConvexHull(points):
+def myConvexHull(points):
     # Menentukan convex hull
+    sorted_points = sorted(util.construct_points(points))
+    total_points = len(sorted_points)
+
+    leftmost_point = sorted_points[0]
+    rightmost_point = sorted_points[total_points-1]
+
+    convex_hull = {leftmost_point, rightmost_point}
+    upper_hull = []
+    lower_hull = []
+
+    for point in sorted_points:
+        distance = util.determinant(leftmost_point, rightmost_point, point)
+        if (distance > 0):
+            upper_hull.append(point)
+        else:
+            lower_hull.append(point)
+
+    util.construct_hull(upper_hull, leftmost_point, rightmost_point, convex_hull)
+    util.construct_hull(lower_hull, rightmost_point, leftmost_point, convex_hull)
+
+    return sorted(convex_hull)
--- src/util.py
+++ src/util.py
@@ -3,29 +3,42 @@ from point import Point
 def construct_points(list_tuples):
     # Membangun point dari list tuple
     list_points = []
-    for t in list_tuples:
-        if isinstance(t, Point):
-            list_points.append(t)
-        else:
-            try:
-                list_points.append(Point(t[0], t[1]))
-            except (IndexError, TypeError):
-                print("Points must have at least 2 coordinates")
+    if list_tuples:
+        for t in list_tuples:
+            if isinstance(t, Point):
+                list_points.append(t)
+            else:
+                try:
+                    list_points.append(Point(t[0], t[1]))
+                except (IndexError, TypeError):
+                    print("Points must have at least 2 coordinates")
     return list_points
 
-def validate_inputs(points):
-    # Memvalidasi input
-    if not hasattr(points, "__iter__"):
-        raise ValueError(f"Expecting an iterable object but got an non-iterable type {points}")
-    
-    if not points:
-        raise ValueError(f"Expecting a list of points but got {points}")
-
-    return construct_points(points)
-
-def determinant(a, b, c):
+def determinant(left, right, point):
     '''
     Menghitung determinan yang nantinya nilainya digunakan untuk mengelompokkan titik c,
     terletak di sebelah kiri (atas) atau sebelah kanan (bawah) garis yang dibentuk titik a dan b
     '''
-    return (a.x * b.y + b.x * c.y + c.x * a.y) - (a.y * b.x + b.y * c.x + c.y * a.x)
+    return (left.x * right.y + right.x * point.y + point.x * left.y) - (left.y * right.x + right.y * point.x + point.y * left.x)
+
+def construct_hull(points, left, right, convex_set):
+    '''
+    Mengupdate convex_set apabila terdapat point yang memenuhi syarat untuk dapat
+    membentuk convex hull 
+    '''
+    extreme_point = None
+    extreme_distance = float("-inf")
+    candidate_points = []
+
+    for point in points:
+        distance = determinant(left, right, point)
+        if (distance > 0):
+            candidate_points.append(point)
+            if (distance > extreme_distance):
+                extreme_distance = distance
+                extreme_point = point
+    
+    if extreme_point:
+        convex_set.add(extreme_point)
+        construct_hull(candidate_points, left, extreme_point, convex_set)
+        construct_hull(candidate_points, extreme_point, right, convex_set)
