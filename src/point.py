4eaf5c6a9c3af87245eb6272f82fd3da6e1db8e4
OjaanIr <farnasrozaan7@gmail.com>
2022-02-26 14:22:05 +0700

    need testing

--- src/point.py
+++ src/point.py
@@ -1,37 +1,67 @@
+import math
+
 class Point:
-    # Kelas point 
+    '''
+    Kelas point 
+    '''
     def __init__(self, x, y):
         self.x = float(x)
         self.y = float(y)
 
-    def __eq__(self, other):
+    def equal(self, other):
+        '''
+        Menghasilkan true jika self == other
+        '''
         return self.x == other.x and self.y == other.y
 
-    def __ne__(self, other):
-        return not self == other
-
-    def __gt__(self, other):
-        if self.x > other.x:
+    def less_than(self, other):
+        '''
+        Menghasilkan True jika self < other
+        '''
+        if (self.x < other.x):
             return True
-        elif self.x == other.x:
+        elif (self.x == other.x):
+            return self.y < other.y
+        return False
+
+    def more_than(self, other):
+        '''
+        Menghasilkan True jika self > other
+        '''
+        if (self.x > other.x):
+            return True
+        elif (self.x == other.x):
             return self.y > other.y
         return False
 
-    def __lt__(self, other):
-        return not self > other
+    def euclid_dist(self, other):
+        '''
+        Menghitung jarak antara dua titik 
+        '''
+        return math.sqrt(pow(self.x-other.x, 2) + (pow(self.y-other.y, 2)))
 
-    def __ge__(self, other):
-        if self.x > other.x:
-            return True
-        elif self.x == other.x:
-            return self.y >= other.y
-        return False
-
-    def __le__(self, other):
-        return not self >= other
+    def farthest_dist_points(self):
+        '''
+        Mencari dua titik yang memiliki jarak terjauh
+        '''
+        n = len(self)
+        max = float("-inf")
+        p1 = None 
+        p2 = None
+        for i in range(n):
+            for j in range(i+1,n):
+                temp1 = self[i]
+                temp2 = self[j]
+                dist = Point.euclid_dist(temp1,temp2)
+                if (max < dist):
+                    max = dist
+                    p1 = temp1
+                    p2 = temp2
+        if (Point.less_than(p1,p2)):
+            return p1, p2
+        else:
+            return p2, p1
+            
 
     def __repr__(self):
-        return f"({self.x}, {self.y})"
-
-    def __hash__(self):
-        return hash(self.x)
+        return f"({self.x}, {self.y})"  
--- src/util.py
+++ src/util.py
@@ -1,27 +1,28 @@
 from point import Point
 
-def construct_points(list_tuples):
-    # Membangun point dari list tuple
+def tuples_to_points(list_tuples):
+    '''
+    Konversi tipe menjadi point
+    '''
     list_points = []
-    if list_tuples:
-        for t in list_tuples:
-            if isinstance(t, Point):
-                list_points.append(t)
-            else:
-                try:
-                    list_points.append(Point(t[0], t[1]))
-                except (IndexError, TypeError):
-                    print("Points must have at least 2 coordinates")
+    for t in list_tuples:
+        if isinstance(t, Point):
+            list_points.append(t)
+        else:
+            try:
+                list_points.append(Point(t[0], t[1]))
+            except (IndexError, TypeError):
+                print("Points must have at least 2 coordinates")
     return list_points
 
 def determinant(left, right, point):
     '''
-    Menghitung determinan yang nantinya nilainya digunakan untuk mengelompokkan titik c,
-    terletak di sebelah kiri (atas) atau sebelah kanan (bawah) garis yang dibentuk titik a dan b
+    Menghitung determinan yang nantinya nilainya digunakan untuk mengelompokkan titik point,
+    terletak di sebelah atas atau bawah garis yang dibentuk titik left dan right 
     '''
     return (left.x * right.y + right.x * point.y + point.x * left.y) - (left.y * right.x + right.y * point.x + point.y * left.x)
 
-def construct_hull(points, left, right, convex_set):
+def convex_hull(points, left, right, convex_set):
     '''
     Mengupdate convex_set apabila terdapat point yang memenuhi syarat untuk dapat
     membentuk convex hull 
@@ -29,16 +30,41 @@ def construct_hull(points, left, right, convex_set):
     extreme_point = None
     extreme_distance = float("-inf")
     candidate_points = []
+    for point in points:
+        if (not (Point.equal(left, point) or Point.equal(right, point))):
+            distance = determinant(left, right, point)
+            if (distance > 0):
+                candidate_points.append(point)
+                if (distance > extreme_distance):
+                    extreme_distance = distance
+                extreme_point = point
+    if extreme_point:
+        convex_hull(candidate_points, left, extreme_point, convex_set)
+        convex_set.add(extreme_point)
+        convex_hull(candidate_points, extreme_point, right, convex_set)
+
+def myConvexHull(points):
+    '''
+    Menentukan convex hull
+    '''
+    points = tuples_to_points(points)
+    total_points = len(points)
+
+    leftmost_point, rightmost_point = Point.farthest_dist_points(points)
+
+    res = {leftmost_point, rightmost_point}
+    upper_hull = []
+    lower_hull = []
 
     for point in points:
-        distance = determinant(left, right, point)
+        distance = determinant(leftmost_point, rightmost_point, point)
         if (distance > 0):
-            candidate_points.append(point)
-            if (distance > extreme_distance):
-                extreme_distance = distance
-                extreme_point = point
-    
-    if extreme_point:
-        convex_set.add(extreme_point)
-        construct_hull(candidate_points, left, extreme_point, convex_set)
-        construct_hull(candidate_points, extreme_point, right, convex_set)
+            upper_hull.append(point)
+        else:
+            lower_hull.append(point)
+
+    # Divide & conquer
+    convex_hull(upper_hull, leftmost_point, rightmost_point, res)
+    convex_hull(lower_hull, rightmost_point, leftmost_point, res)
+
+    return res
